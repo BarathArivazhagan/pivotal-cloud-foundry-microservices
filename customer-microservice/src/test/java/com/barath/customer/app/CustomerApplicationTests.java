c863129b7506f29e15804a67e202edbe2062bfe2
BarathArivazhagan <barathmacdec91@gmail.com>
2018-03-25 17:46:56 +0530

    modified manifest.yml

--- customer-microservice/src/test/java/com/barath/customer/app/CustomerApplicationTests.java
+++ customer-microservice/src/test/java/com/barath/customer/app/CustomerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(properties={"spring.cloud.discovery.enabled = false"})
 public class CustomerApplicationTests {
 	
 	
