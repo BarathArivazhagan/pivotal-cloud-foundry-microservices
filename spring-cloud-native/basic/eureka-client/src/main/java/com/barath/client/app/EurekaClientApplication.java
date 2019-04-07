24d66f2ff87dc841566eaaf3c8a4b831293ef0f1
BarathArivazhagan <barathmacdec91@gmail.com>
2019-04-07 13:56:28 +0530

    spring boot upgrade to 2.1.4.RELEASE

--- spring-cloud-native/basic/eureka-client/src/main/java/com/barath/client/app/EurekaClientApplication.java
+++ spring-cloud-native/basic/eureka-client/src/main/java/com/barath/client/app/EurekaClientApplication.java
@@ -3,6 +3,9 @@ package com.barath.client.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.ResponseEntity;
+
+import javax.xml.ws.Response;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +13,6 @@ public class EurekaClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
+
 	}
 }
--- spring-cloud-native/basic/eureka-server/src/main/java/com/barath/eureka/app/EurekaServerApplication.java
+++ spring-cloud-native/basic/eureka-server/src/main/java/com/barath/eureka/app/EurekaServerApplication.java
@@ -3,6 +3,10 @@ package com.barath.eureka.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,4 +15,7 @@ public class EurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
+
+
 }
+
